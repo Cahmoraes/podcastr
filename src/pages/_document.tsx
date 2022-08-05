d15b1cd1b2eef9e553e6e518723f9ce25538ee8c
caique.moraes <caique.moraes@compasso.com.br>
2022-08-05 14:20:31 -0300

    add tag meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </Head>
       <body>
